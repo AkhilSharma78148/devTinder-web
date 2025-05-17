606d030013fd511c56e113888a96a9fec0eaf11b
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-05-17 16:46:30 +0530

    useEffect and profile view

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utlis/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utlis/userSlice'
+import { useEffect } from 'react'
 
 const Body = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      if(userData) return;
+
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch(err) {
+      if(err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+  
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+
   return (
     <div>
         <NavBar />

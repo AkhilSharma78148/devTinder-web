7d92cdcb9e156f2058771ac3dfca9b1ae8bae362
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-06-05 20:54:03 +0530

    lazy loading chunk size

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,39 +1,46 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from '../utlis/todoSlice';
 
-const Todo = () => {
-    const dispatch = useDispatch();
-    const todos = useSelector((store) => store.todos);
+const CHUNK_SIZE = 5;
 
-    const fetchTodos = async () => {
-        if(todos) return;
-        try {
-            const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-            dispatch(addTodo(response.data)); 
-        } catch (err) {
-            console.log(err);
-        }
-    };
+const Todo = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((store) => store.todos) || [];
+
+  const [visibleCount, setVisibleCount] = useState(CHUNK_SIZE);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      dispatch(addTodo(response.data)); 
+    } catch (err) {
+      console.error('Error fetching todos:', err);
+    }
+  };
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + CHUNK_SIZE);
+  };
+
   return (
     <div className="text-center my-10">
         <h1 className="text-bold text-5xl">Todos</h1>
       <ul>
-        {todos?.map((todo) => {
-          const { id, title, completed } = todo;
-          return (
-            <li key={id} className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto card-title">
-              {title} - {completed ? 'Yes' : 'No'}
-            </li>
-          );
-        })}
+        {todos.slice(0, visibleCount).map((todo) => (
+          <li className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto" key={todo.id}>
+            {todo.title} - {todo.completed ? 'Yes' : 'No'}
+          </li>
+        ))}
       </ul>
+      {visibleCount < todos.length && (
+        <button onClick={handleLoadMore}>Load More</button>
+      )}
     </div>
   );
 };
